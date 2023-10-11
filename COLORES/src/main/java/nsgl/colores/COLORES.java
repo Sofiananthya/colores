0f609a418e1cb677f218d1fc57486fa468005a3b
Sofiananthya <145470302+Sofiananthya@users.noreply.github.com>
2023-10-11 07:08:07 -0600

    Add files via upload

---
+++ COLORES/src/main/java/nsgl/colores/COLORES.java
@@ -0,0 +1,29 @@
+Private void btnColorearMouseClicked(javax.awt.event.MouseEvent){
+
+        //TODO add your handling code here;
+        lblColor.setOpaque(isopaque:true);
+
+        if(rbtnRojo.isSelected())
+        {
+          lblColor.setBackground(bg: Color.red);
+        }
+        if(rbtnVerde.isSelected())
+        {
+          lblColor.setBachground(bg: Color.GREEN);
+        }
+        if(rbtnAzul.isSelected(sss))
+}
+}
+
+package nsgl.colores;
+
+/**
+ *
+ * @author User
+ */
+public class COLORES {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
